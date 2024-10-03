9bc8629aed9f18b730e0e057650efbecb3a96e0f
Saifu <trksaifuyt@gmail.com>
2024-10-03 23:45:01 +0530

    Added Day-1 DSA solutions

---
+++ Day 1/problem2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int n = nums.length;
+
+        int pre = 1, suff = 1;
+        int ans = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            if (pre == 0) pre = 1;
+            if (suff == 0) suff = 1;
+            pre *= nums[i];
+            suff *= nums[n - i - 1];
+            ans = Math.max(ans, Math.max(pre, suff));
+        }
+        return ans;
+    }
+}
