00e6340ea5b8a666b2403c85b66b2e0a13fb4e6f
Saifu <trksaifuyt@gmail.com>
2024-10-04 11:55:58 +0530

    Day -3 done

---
+++ Day 3/problem2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int maxi=0, count=0;
+
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] == 1){
+                count++;
+            }else{
+                count = 0;
+            }
+            maxi = Math.max(maxi, count);
+        }
+        return maxi;
+    }
+}
