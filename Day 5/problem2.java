fffced0672e1fee2bcc7fe71c5aad2342157dce5
Saifu <trksaifuyt@gmail.com>
2024-10-06 23:36:42 +0530

    Day-5 done

---
+++ Day 5/problem2.java
@@ -0,0 +1,23 @@
+public class BS_UpperBound {
+    static int upperBoundOptimal(int[] arr, int n, int x) {
+        int low = 0, high = n - 1;
+        int ans = n;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] > x) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 5, 8, 9, 15, 19};
+        int n = arr.length;
+        System.out.println("The upper bound is the index: "+upperBoundOptimal(arr,n,9));
+    }
+}
