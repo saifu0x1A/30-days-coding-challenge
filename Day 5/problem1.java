fcb33f4b7b314abc109ec9052878a7f4f6872027
Saifu <trksaifuyt@gmail.com>
2024-10-06 23:33:47 +0530

    Day-5 done

---
+++ Day 5/problem1.java
@@ -0,0 +1,18 @@
+public class BS_LowerBound {
+    static int lowerBoundOptimal(int[] arr, int target) {
+        int n = arr.length;
+        int low = 0, high = n - 1;
+        int ans = n;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] >= target) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
