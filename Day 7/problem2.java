8a787d5d2a9a701859437738905dac5daf0de21b
Saifu <trksaifuyt@gmail.com>
2024-10-08 17:57:43 +0530

    Day-7 done (one week completed successfully)

---
+++ Day 7/problem2.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int n = nums.length;
+        for(int i=0; i<n; i++){
+            if(nums[i] == target) return true;
+        }
+        return false;
+    }
+}
