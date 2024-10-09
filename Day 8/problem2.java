cfb862b78472abb7c560508237bd31f50919b107
Saifu <trksaifuyt@gmail.com>
2024-10-09 23:53:31 +0530

    Initial commit

--- Day 8/problem2.java
+++ Day 8/problem2.java
@@ -1,6 +1,6 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        int left = 0, right = nums.length-1;
+        int left = 0, right = nums.length - 1;
         while(left < right){
             int mid = left + (right - left)/2;
             if(nums[mid] > nums[mid+1]){
