c1dd88630da45c50577be51b20a0df41715c6ebf
Saifu <trksaifuyt@gmail.com>
2024-10-11 19:16:10 +0530

    Day-10 done

---
+++ Day 10/problem1.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int smallestDivisor(int[] nums, int threshold) {
+        int n = nums.length;
+        if(n > threshold) return -1;
+
+        int maxi = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            maxi = Math.max(maxi, nums[i]);
+        }
+
+        int low = 1, high = maxi;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (sumByD(nums, mid) <= threshold) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+    static int sumByD(int[] arr, int div) {
+        int n = arr.length;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += Math.ceil((double)(arr[i]) / (double)(div));
+        }
+        return sum;
+    }
+}
