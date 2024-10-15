92ed2d99ae586ab3c805279ef8c15a6c45b73b2e
Saifu <trksaifuyt@gmail.com>
2024-10-15 23:19:05 +0530

    Day-14 done

---
+++ Day 14/problem2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int len = flowerbed.length;
+        int i = 0;
+        while(i < len && n > 0) {
+            if(flowerbed[i] == 1) {
+                i += 2;
+            }else if(i == len - 1 || flowerbed[i + 1] == 0) {
+                n--;
+                i += 2;
+            }
+            else i += 3;
+        }
+        return n <= 0;
+    }
+}
+
+
